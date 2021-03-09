93d9e431ed72f162dfbe94340f649f136acb3611
ITmxs <17752170152@163.com>
2021-03-09 21:18:48 +0800

    ExperienceApp

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,9 @@ import 'package:flutter/material.dart';
 
 import 'ep701_clubhouse/ep701_clubhouse_clone.dart';
 import 'ep714_food_bill_app/food_bill_app.dart';
+import 'ep718_experience_app/experience_app.dart';
 
 void main() {
   //食物app
-  runApp(FoodBillApp());
+  runApp(ExperienceApp());
 }
