213b8dcebb4c53ecbff0465b0b0d89271753b062
ITmxs <17752170152@163.com>
2021-03-09 21:20:30 +0800

    VPp

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,9 @@ import 'package:flutter/material.dart';
 import 'ep701_clubhouse/ep701_clubhouse_clone.dart';
 import 'ep714_food_bill_app/food_bill_app.dart';
 import 'ep718_experience_app/experience_app.dart';
+import 'ep721_vpn_mobile_app/vpn_mobile_app.dart';
 
 void main() {
-  //食物app
-  runApp(ExperienceApp());
+  //Vpn
+  runApp(VpnMobileApp());
 }
